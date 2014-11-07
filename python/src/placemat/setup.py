08284246820bea7c03fd362e17e58280d9b26b4d
Ian <ian@placemat.me.uk>
2014-11-07 08:45:17 +0000

    placemat: 	Added setup.py script and changed package structure - now installable with python setup.py

---
+++ python/src/placemat/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(	name='placemat',
+	version='1.0.0',
+	description='Some handy Python snippets',
+	author='Ian Ager',
+	author_email='ian@placemat.me.uk',
+	url='http://placemat.me.uk',
+	packages=['placemat']
+)
